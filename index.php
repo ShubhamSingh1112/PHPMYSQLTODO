206d751cf6948ba5cab19e61fb0e564e097af059
Shubham Singh <46527607+ShubhamSingh1112@users.noreply.github.com>
2022-10-09 23:23:53 +0530

    Upgrading Assets
    Changing Bootstrap version

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>TODO</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</head>
-<body>
-
-<?php require_once 'process.php'; ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <?php require_once 'process.php'; ?>
 
 <?php 
 if(isset($_SESSION['message'])):
  ?>
-<div class="alert alert-<?=$_SESSION['msg_type']?>"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<div class="alert alert-<?=$_SESSION['msg_type']?>"><a href="#" class="close" data-bs-dismiss="alert" aria-label="close">&times;</a>
 <?php 
 echo $_SESSION['message'];
 unset($_SESSION['message']);
@@ -78,5 +76,6 @@ unset($_SESSION['message']);
 	    </div>
 	</form>
 </div>
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
 </html>
